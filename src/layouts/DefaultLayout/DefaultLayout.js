0f9c3b9a563c3fd3a09d53358793a8c6c28b6632
Minh-Ngoc <ngocminh101100@gmail.com>
2023-07-29 22:39:24 +0700

    update code

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -3,18 +3,10 @@ import classNames from 'classnames/bind';
 import styles from './DefaultLayout.module.scss';
 import Header from 'components/Header';
 import Sidebar from 'components/Sidebar';
-import Cookies from "js-cookie";
-import { useNavigate } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
-    const navigate = useNavigate();
-    const token = Cookies.get("token");
-    console.log(token)
-    if(!token) {
-        return navigate("/login");
-    }
 
     return ( 
         <Wrapper className={cx('layout')}>
