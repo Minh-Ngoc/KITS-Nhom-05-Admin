48576fa1a54dffb5c3943a225af3e7005bf05fdb
Minh-Ngoc <ngocminh101100@gmail.com>
2023-07-30 10:46:51 +0700

    set token in default layout

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -3,22 +3,39 @@ import classNames from 'classnames/bind';
 import styles from './DefaultLayout.module.scss';
 import Header from 'components/Header';
 import Sidebar from 'components/Sidebar';
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
+    const token = Cookies.get('token');
 
-    return ( 
-        <Wrapper className={cx('layout')}>
-            <Header />
-            <div className={cx('main')}>
-                <Sidebar />
-                <div className={cx('container')}>
-                    {children}
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!token) {
+            return navigate("/login");
+        }
+    })
+
+    if(!token) {
+        <div>Loading...</div>
+    } else {
+        return ( 
+            <Wrapper className={cx('layout')}>
+                <Header />
+                <div className={cx('main')}>
+                    <Sidebar />
+                    <div className={cx('container')}>
+                        {children}
+                    </div>
                 </div>
-            </div>
-        </Wrapper>
-    );
+            </Wrapper>
+        );
+    }
+
 }
 
 export default DefaultLayout;
